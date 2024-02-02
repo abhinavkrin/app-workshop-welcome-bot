0685ea82848d6e4e075d6016ceb67ae6eab2d21b
Abhinav Kumar <abhinav@avitechlab.com>
2024-02-02 19:27:56 +0530

    added getApiUrl
    Signed-off-by: Abhinav Kumar <abhinav@avitechlab.com>

--- WelcomeBotApp.ts
+++ WelcomeBotApp.ts
@@ -7,6 +7,9 @@ import {
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
 import { GetInfo } from './slashcommands/GetInfo';
+
+const getApiUrl = username => `https://welcome-gen-59cce07ae427.herokuapp.com/welcome/${username}`;
+
 export class WelcomeBotApp extends App {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
